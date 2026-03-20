fb51572b30123cecba6fd34c9e864c3db88f44b7
Ronan POTIER <ronanpotier@gmail.com>
2026-03-20 03:58:41 +0100

    pages with different topics added

---
+++ dashboard/pages/2_jobs.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import pandas as pd
+
+print("here will be analysis about Jobs and qualifications")
--- dashboard/streamlit.py
+++ dashboard/streamlit.py
@@ -1,6 +1,14 @@
 import streamlit as st
-st.title("CRIT_DASHBOARD")
+import pandas as pd
+
+
+st.title("CRIT_DASHBOARD EVENT")
 st.write("Premier texte du dashboard")
 if st.checkbox("AFFICHER") :
 	st.write("ICI DU TEXTE DEVRAIS S'AFFICHER")
+
+
+df = pd.read_csv("/home/prime/Projets/2026_PROJECT-DA/DATA/new_dataset.csv")
+st.dataframe(df)
+
 
