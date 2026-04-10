f6d955e305403265916bfa1ac8d106e474c8a473
Ronan-ptr <ronanpotier@gmail.com>
2026-04-10 19:20:53 +0200

    final app should working

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,11 @@
 import pandas as pd
-#import matplotlib as plt
-#import seaborn as sns
+import matplotlib as plt
+import seaborn as sns
 
 print("hellp world")
+
+
+df = pd.read_csv("../data/clean_dataset.csv")
+
+df.head()
+
